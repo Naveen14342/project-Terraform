747e6c56e3c8b968632118cfaad1b112dcef8480
naveen <naveentharapatla@gmail.com>
2026-02-23 21:54:08 +0530

    add correct  az to region

--- env/test4/main.tf
+++ env/test4/main.tf
@@ -18,7 +18,7 @@ module "aws_vpc" {
 
   name = "my-vpc-south-virginia"
   cidr = "10.0.0.0/16"
-  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs            = ["us-east-2a", "us-east-2b", "us-east-2c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway = true
