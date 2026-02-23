152c07436e3f6fc73dea6760923113cf60c0040d
naveen <naveentharapatla@gmail.com>
2026-02-23 19:32:12 +0530

    adding new files

---
+++ env/dev/code.tf
@@ -0,0 +1,43 @@
+terraform {
+  backend "s3" {
+  bucket = "terraform-states-prod1"
+  key    = "dev/terraform.tfstate"
+  region = "us-east-1"
+
+  role_arn = "arn:aws:iam::907143134003:role/TerraformStateAccessRole"
+}
+}
+
+
+module "aws_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+  
+
+  name = "my-vpc-north-virginia"
+  cidr = "10.0.0.0/16"
+  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = "dev"
+    project     = "my-project"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/my-cluster" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/my-cluster" = "shared"
+  }
+
+}
